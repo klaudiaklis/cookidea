5a264e8a46d825c6bcdaf2a1ada2759f179d286b
klaudiaklis <30574178+klaudiaklis@users.noreply.github.com>
2017-08-02 23:20:27 +0200

    insert data to database
    added category and products to databese script

--- create_db.sql
+++ create_db.sql
@@ -18,6 +18,11 @@ CREATE TABLE uncountableProductCategory(
 	name VARCHAR(20)
 );
 
+CREATE TABLE binaryProductCategory(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(20)
+);
+
 CREATE TABLE uncountableProduct (
 	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	name VARCHAR(100), 
@@ -37,6 +42,13 @@ CREATE TABLE countableProduct(
 	name VARCHAR(100), 
 	countableProductCategoryId INTEGER, 
 	FOREIGN KEY(countableProductCategoryId) REFERENCES countableProductCategory(id)
+);
+
+CREATE TABLE binaryProduct(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(100), 
+	binaryProductCategoryId INTEGER, 
+	FOREIGN KEY(binaryProductCategoryId) REFERENCES binaryProductCategory(id)
 );
 
 CREATE TABLE household(
@@ -62,14 +74,51 @@ CREATE TABLE recipe (
 	duration INTEGER, 
 	mealTypeId INTEGER NOT NULL, 
 	cousineTypeId INTEGER NOT NULL,  
+	portions INTEGER NOT NULL,
+	difficultyLevel INTEGER NOT NULL,
 	FOREIGN KEY(mealTypeId) REFERENCES mealType(id), 
 	FOREIGN KEY(cousineTypeId) REFERENCES cousineType(id)
+);
+
+CREATE TABLE liquidProductRecipe (
+	liquidProductId INTEGER NOT NULL,
+	recipeId INTEGER NOT NULL,
+	volume DOUBLE, 
+	PRIMARY KEY(liquidProductId,recipeId),
+	FOREIGN KEY(liquidProductId) REFERENCES liquidProduct(id),
+	FOREIGN KEY(recipeId) REFERENCES recipe(id)
+);
+
+CREATE TABLE countableProductRecipe (
+	countableProductId INTEGER NOT NULL,
+	recipeId INTEGER NOT NULL,
+	volume DOUBLE, 
+	PRIMARY KEY(countableProductId,recipeId),
+	FOREIGN KEY(countableProductId) REFERENCES countableProduct(id),
+	FOREIGN KEY(recipeId) REFERENCES recipe(id)
+);
+
+CREATE TABLE uncountableProductRecipe (
+	uncountableProductId INTEGER NOT NULL,
+	recipeId INTEGER NOT NULL,
+	volume DOUBLE, 
+	PRIMARY KEY(uncountableProductId,recipeId),
+	FOREIGN KEY(uncountableProductId) REFERENCES uncountableProduct(id),
+	FOREIGN KEY(recipeId) REFERENCES recipe(id)
+);
+
+CREATE TABLE binaryProductRecipe (
+	binaryProductId INTEGER NOT NULL,
+	recipeId INTEGER NOT NULL,
+	PRIMARY KEY(binaryProductId,recipeId),
+	FOREIGN KEY(binaryProductId) REFERENCES binaryProduct(id),
+	FOREIGN KEY(recipeId) REFERENCES recipe(id)
 );
 
 CREATE TABLE liquidProductHousehold (
 	liquidProductId INTEGER NOT NULL,
 	householdId INTEGER NOT NULL,
-	volume DECIMAL, 
+	volume DOUBLE, 
 	PRIMARY KEY(liquidProductId,householdId),
 	FOREIGN KEY(liquidProductId) REFERENCES liquidProduct(id),
 	FOREIGN KEY(householdId) REFERENCES household(id)
@@ -87,15 +136,88 @@ CREATE TABLE countableProductHousehold(
 CREATE TABLE uncountableProductHousehold(
 	uncountableProductId INTEGER NOT NULL,
 	householdId INTEGER NOT NULL,
-	weight DECIMAL, 
+	weight DOUBLE, 
 	PRIMARY KEY(uncountableProductId,householdId),
 	FOREIGN KEY(uncountableProductId) REFERENCES uncountableProduct(id),
 	FOREIGN KEY(householdId) REFERENCES household(id)
 );
 
-INSERT INTO liquidProductCategory VALUES(1,'sosy');
-INSERT INTO household VALUES(1,'domKlaudii','haslo');
-INSERT INTO liquidProduct VALUES(1,'sojowy',1);
+CREATE TABLE binaryProductHousehold(
+	binaryProductId INTEGER NOT NULL,
+	householdId INTEGER NOT NULL,
+	availability BOOLEAN, 
+	PRIMARY KEY(uncountableProductId,householdId),
+	FOREIGN KEY(uncountableProductId) REFERENCES uncountableProduct(id),
+	FOREIGN KEY(householdId) REFERENCES household(id)
+);
+
+INSERT INTO countableProductCategory VALUES(default, 'Fruits');
+INSERT INTO countableProductCategory VALUES(default, 'Vegetagles');
+INSERT INTO countableProductCategory VALUES(default, 'Dairy');
+INSERT INTO countableProductCategory VALUES(default, 'Bread');
+
+INSERT INTO uncountableProductCategory VALUES(default, 'Meats');
+INSERT INTO uncountableProductCategory VALUES(default, 'Fish');
+INSERT INTO uncountableProductCategory VALUES(default, 'Seafood');
+INSERT INTO uncountableProductCategory VALUES(default, 'Grain products');
+INSERT INTO uncountableProductCategory VALUES(default, 'Dairy');
+
+INSERT INTO liquidProductCategory VALUES(default, 'Dairy');
+INSERT INTO liquidProductCategory VALUES(default, 'Oils');
+INSERT INTO liquidProductCategory VALUES(default, 'Alcohols');
+
+INSERT INTO binaryProductCategory VALUES(default, 'Spices');
+
+INSERT INTO household VALUES(default,'domKlaudii','haslo');
+
+INSERT INTO countableProduct VALUES(default, 'tomato', 2)
+INSERT INTO countableProduct VALUES(default, 'carrot', 2)
+INSERT INTO countableProduct VALUES(default, 'parsley', 2)
+INSERT INTO countableProduct VALUES(default, 'celery', 2)
+INSERT INTO countableProduct VALUES(default, 'onion', 2)
+INSERT INTO countableProduct VALUES(default, 'garlic', 2)
+INSERT INTO countableProduct VALUES(default, 'cabbage', 2)
+INSERT INTO countableProduct VALUES(default, 'egg', 3)
+INSERT INTO countableProduct VALUES(default, 'roll', 4)
+INSERT INTO countableProduct VALUES(default, ' ', )
+INSERT INTO countableProduct VALUES(default, ' ', )
+INSERT INTO countableProduct VALUES(default, ' ', )
+
+INSERT INTO uncountableProduct VALUES(default,'pasta spaghetti', 4);
+INSERT INTO uncountableProduct VALUES(default, 'beef', 1)
+INSERT INTO uncountableProduct VALUES(default, 'cheese Grana Padano', 5)
+INSERT INTO uncountableProduct VALUES(default, 'rice', 4)
+INSERT INTO uncountableProduct VALUES(default, 'butter', 5)
+INSERT INTO uncountableProduct VALUES(default, 'cheese Cheddar', 5)
+INSERT INTO uncountableProduct VALUES(default, 'chicken', 1)
+INSERT INTO uncountableProduct VALUES(default, 'pork chop', 1)
+INSERT INTO uncountableProduct VALUES(default, '', )
+INSERT INTO uncountableProduct VALUES(default, ' ', )
+INSERT INTO uncountableProduct VALUES(default, ' ', )
+INSERT INTO uncountableProduct VALUES(default, ' ', )
+
+INSERT INTO liquidProduct VALUES(default, 'olive oil', 2)
+INSERT INTO liquidProduct VALUES(default, 'red wine', 3)
+INSERT INTO liquidProduct VALUES(default, 'white wine', 3)
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, ' ', )
+
+INSERT INTO binaryProduct VALUES(default, 'bay leaf', 1)
+INSERT INTO binaryProduct VALUES(default, 'black pepper', 1)
+INSERT INTO binaryProduct VALUES(default, 'salt', 1)
+INSERT INTO binaryProduct VALUES(default, 'allspice', 1)
+INSERT INTO binaryProduct VALUES(default, 'basil', 1)
+INSERT INTO binaryProduct VALUES(default, ' ', )
+INSERT INTO binaryProduct VALUES(default, ' ', )
+
 INSERT INTO liquidProductHousehold VALUES(1,1,0.3);
 
 
