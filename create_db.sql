c8b65f4cbb60a3546f971fcee3554982aca0ed3e
klaudiaklis <30574178+klaudiaklis@users.noreply.github.com>
2017-08-03 21:43:04 +0200

    DB script finished
    DB script finished

--- create_db.sql
+++ create_db.sql
@@ -155,70 +155,187 @@ INSERT INTO countableProductCategory VALUES(default, 'Fruits');
 INSERT INTO countableProductCategory VALUES(default, 'Vegetagles');
 INSERT INTO countableProductCategory VALUES(default, 'Dairy');
 INSERT INTO countableProductCategory VALUES(default, 'Bread');
+INSERT INTO countableProductCategory VALUES(default, 'Sweets');
 
 INSERT INTO uncountableProductCategory VALUES(default, 'Meats');
 INSERT INTO uncountableProductCategory VALUES(default, 'Fish');
 INSERT INTO uncountableProductCategory VALUES(default, 'Seafood');
 INSERT INTO uncountableProductCategory VALUES(default, 'Grain products');
 INSERT INTO uncountableProductCategory VALUES(default, 'Dairy');
+INSERT INTO uncountableProductCategory VALUES(default, 'Sweets');
 
 INSERT INTO liquidProductCategory VALUES(default, 'Dairy');
 INSERT INTO liquidProductCategory VALUES(default, 'Oils');
 INSERT INTO liquidProductCategory VALUES(default, 'Alcohols');
 
 INSERT INTO binaryProductCategory VALUES(default, 'Spices');
+INSERT INTO binaryProductCategory VALUES(default, 'Sauce');
 
 INSERT INTO household VALUES(default,'domKlaudii','haslo');
 
-INSERT INTO countableProduct VALUES(default, 'tomato', 2)
-INSERT INTO countableProduct VALUES(default, 'carrot', 2)
-INSERT INTO countableProduct VALUES(default, 'parsley', 2)
-INSERT INTO countableProduct VALUES(default, 'celery', 2)
-INSERT INTO countableProduct VALUES(default, 'onion', 2)
-INSERT INTO countableProduct VALUES(default, 'garlic', 2)
-INSERT INTO countableProduct VALUES(default, 'cabbage', 2)
-INSERT INTO countableProduct VALUES(default, 'egg', 3)
-INSERT INTO countableProduct VALUES(default, 'roll', 4)
-INSERT INTO countableProduct VALUES(default, ' ', )
-INSERT INTO countableProduct VALUES(default, ' ', )
-INSERT INTO countableProduct VALUES(default, ' ', )
+INSERT INTO countableProduct VALUES(default, 'tomato', 2);
+INSERT INTO countableProduct VALUES(default, 'carrot', 2);
+INSERT INTO countableProduct VALUES(default, 'parsley', 2);
+INSERT INTO countableProduct VALUES(default, 'celery', 2);
+INSERT INTO countableProduct VALUES(default, 'onion', 2);
+INSERT INTO countableProduct VALUES(default, 'garlic', 2);
+INSERT INTO countableProduct VALUES(default, 'cabbage', 2);
+INSERT INTO countableProduct VALUES(default, 'egg', 3);
+INSERT INTO countableProduct VALUES(default, 'roll', 4);
+INSERT INTO countableProduct VALUES(default, 'Iceberg lettuce', 2);
+INSERT INTO countableProduct VALUES(default, 'cucamber', 2);
+INSERT INTO countableProduct VALUES(default, 'dark chockolate', 5);
+INSERT INTO countableProduct VALUES(default, 'white chockolate', 5);
+INSERT INTO countableProduct VALUES(default, 'banana', 1);
+INSERT INTO countableProduct VALUES(default, 'kiwi', 1);
+INSERT INTO countableProduct VALUES(default, 'strawberry', 1);
+INSERT INTO countableProduct VALUES(default, 'apple', 1);
+INSERT INTO countableProduct VALUES(default, 'orange', 1);
+INSERT INTO countableProduct VALUES(default, 'lemon', 1);
+INSERT INTO countableProduct VALUES(default, 'raspberry', 1);
 
 INSERT INTO uncountableProduct VALUES(default,'pasta spaghetti', 4);
-INSERT INTO uncountableProduct VALUES(default, 'beef', 1)
-INSERT INTO uncountableProduct VALUES(default, 'cheese Grana Padano', 5)
-INSERT INTO uncountableProduct VALUES(default, 'rice', 4)
-INSERT INTO uncountableProduct VALUES(default, 'butter', 5)
-INSERT INTO uncountableProduct VALUES(default, 'cheese Cheddar', 5)
-INSERT INTO uncountableProduct VALUES(default, 'chicken', 1)
-INSERT INTO uncountableProduct VALUES(default, 'pork chop', 1)
-INSERT INTO uncountableProduct VALUES(default, '', )
-INSERT INTO uncountableProduct VALUES(default, ' ', )
-INSERT INTO uncountableProduct VALUES(default, ' ', )
-INSERT INTO uncountableProduct VALUES(default, ' ', )
+INSERT INTO uncountableProduct VALUES(default, 'beef', 1);
+INSERT INTO uncountableProduct VALUES(default, 'cheese Grana Padano', 5);
+INSERT INTO uncountableProduct VALUES(default, 'rice', 4);
+INSERT INTO uncountableProduct VALUES(default, 'butter', 5);
+INSERT INTO uncountableProduct VALUES(default, 'cheese Cheddar', 5);
+INSERT INTO uncountableProduct VALUES(default, 'chicken', 1);
+INSERT INTO uncountableProduct VALUES(default, 'pork chop', 1);
+INSERT INTO uncountableProduct VALUES(default, 'bacon', 1);
+INSERT INTO uncountableProduct VALUES(default, 'sugar', 6);
+INSERT INTO uncountableProduct VALUES(default, 'flour', 4);
+INSERT INTO uncountableProduct VALUES(default, 'Mascarpone cheese', 5);
 
-INSERT INTO liquidProduct VALUES(default, 'olive oil', 2)
-INSERT INTO liquidProduct VALUES(default, 'red wine', 3)
-INSERT INTO liquidProduct VALUES(default, 'white wine', 3)
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
-INSERT INTO liquidProduct VALUES(default, ' ', )
+INSERT INTO liquidProduct VALUES(default, 'olive oil', 2);
+INSERT INTO liquidProduct VALUES(default, 'red wine', 3);
+INSERT INTO liquidProduct VALUES(default, 'white wine', 3);
+INSERT INTO liquidProduct VALUES(default, 'buttermilk', 1);
+INSERT INTO liquidProduct VALUES(default, 'milk', 1);
 
-INSERT INTO binaryProduct VALUES(default, 'bay leaf', 1)
-INSERT INTO binaryProduct VALUES(default, 'black pepper', 1)
-INSERT INTO binaryProduct VALUES(default, 'salt', 1)
-INSERT INTO binaryProduct VALUES(default, 'allspice', 1)
-INSERT INTO binaryProduct VALUES(default, 'basil', 1)
-INSERT INTO binaryProduct VALUES(default, ' ', )
-INSERT INTO binaryProduct VALUES(default, ' ', )
+INSERT INTO binaryProduct VALUES(default, 'bay leaf', 1);
+INSERT INTO binaryProduct VALUES(default, 'black pepper', 1);
+INSERT INTO binaryProduct VALUES(default, 'salt', 1);
+INSERT INTO binaryProduct VALUES(default, 'allspice', 1);
+INSERT INTO binaryProduct VALUES(default, 'basil', 1);
+INSERT INTO binaryProduct VALUES(default, 'oregano', 1);
+INSERT INTO binaryProduct VALUES(default, 'ketchup', 2);
+INSERT INTO binaryProduct VALUES(default, 'Barbecue sauce', 2);
+INSERT INTO binaryProduct VALUES(default, 'Tomato pasado', 2);
 
-INSERT INTO liquidProductHousehold VALUES(1,1,0.3);
+INSERT INTO mealType VALUES(default, 'Breakfast');
+INSERT INTO mealType VALUES(default, 'Lunch');
+INSERT INTO mealType VALUES(default, 'Dinner');
+INSERT INTO mealType VALUES(default, 'Dessert');
+INSERT INTO mealType VALUES(default, 'Supper');
+
+INSERT INTO cousineType VALUES(default, 'Poland');
+INSERT INTO cousineType VALUES(default, 'American');
+INSERT INTO cousineType VALUES(default, 'Italian');
+INSERT INTO cousineType VALUES(default, 'Czech');
+INSERT INTO cousineType VALUES(default, 'French');
+INSERT INTO cousineType VALUES(default, 'Asian');
+
+INSERT INTO recipe VALUES(default, 'Spaghetti bolognese','Na patelnię wlewamy oliwę. Od razu dodajemy warzywa: pokrojoną w kostkę marchewkę, cebulę, seler naciowy oraz przeciśnięty przez praskę czosnek. Podsmażamy na niewielkim ogniu, co chwilę mieszając. Następnie dodajemy mięso mielone i smażymy do zarumienienia. Dolewamy wino i gotujemy, aż alkohol odparuje. Następnie dodajemy pomidory pelati. Doprawiamy solą, pieprzem, niewielką ilością cukru oraz porwanymi listkami bazylii. W dużym garnku zagotowujemy wodę. Do gotującej się wody dodajemy sól i makaron. Gotujemy al dente według wskazań na opakowaniu. Makaron odcedzamy przez sito. Niewielką ilość wody, w której gotował się makaron, wlewamy do sosu. Ugotowany makaron dokładnie mieszamy z sosem. Makaron z sosem przekładamy na talerz. Wierzch posypujemy startym parmezanem. Dekorujemy listkami świeżej bazylii.', 3, 3, 4, 4);
+INSERT INTO recipe VALUES(default, 'Risotto','Dymkę (bez szczypioru) drobno siekamy i szklimy na rozgrzanym maśle. Do cebuli dodajemy suchy ryż, całość mieszamy. Wino mieszamy z musztardą i wlewamy na patelnię. Smażymy na dużym ogniu, aż ryż wchłonie wino, nieustannie mieszając. Do ryżu dodajemy szklankę ugotowanego bulionu z kurczaka, mieszamy. Gdy ryż wchłonie płyn, dodajemy kolejną szklankę bulionu. Czynność powtarzamy, aż cały bulion zostanie dodany. Gdy ryż będzie już lekko ugotowany, dodajemy starty na tarce cheddar i mieszamy, aż ser się roztopi. Przyprawiamy do smaku solą i pieprzem.', 3, 3, 4, 3);
+INSERT INTO recipe VALUES(default, 'Doves', 'Mięso włożyć do większej miski. Ryż ugotować, przelać zimną wodą na sicie, odsączyć i wystudzony dodać do mięsa. Cebulę obrać, zetrzeć na tarce, dodać do mięsa z ryżem. Doprawić solą (około pół łyżeczki), pieprzem (1/4 łyżeczki). Wszystko wymieszać i dobrze wyrobić dłonią. Uformować niewielkie podłużne kotlety. Czytaj dalej...', 3, 1, 6, 5);
+INSERT INTO recipe VALUES(default, 'Burgers', 'Mięso zmielić, wymieszać z cebulą i doprawić solą i pieprzem. Uformować kotlety i smażyć razem z boczkiem na patelni grilowej przez 20min. Pozostałe warzywa pokroić. Na podgrzane bułki ułożyć kolejno mięso, warzywa i udekorować sosem', 2, 2, 6, 2);
+INSERT INTO recipe VALUES(default, 'Brownie', 'Rozpuścić masło, czekoladę i cukier w kąpieli wodnej. Jajka roztrzep w misce jak na jajecznicę, dodaj mąkę, wymieszaj rózgą lub mikserem. Cały czas mieszając, wolnym strumieniem wlewaj do jajek czekoladę. Piecz 25 minut w temperaturze 180 stopni.', 1, 2, 6, 2);
+INSERT INTO recipe VALUES(default, 'Pancakes', 'W blenderze zmiksować wszystkie składniki na gładką masę o konsystencji gęstej śmietany. Rozgrzać patelnię i na średnim ogniu smażyć pancakes z dwóch stron. Podawać z owocami', 4, 2, 24, 2);
+INSERT INTO recipe VALUES(default, 'Fruits salat', 'Wszystkie owoce umyć, obrać i pokroić w kostkę. Wydusić sok z jednej limonki i wymieszać delikatnie wszystko. Schłodzić przed podaniem. ', 5, 1, 2, 1);
+
+INSERT INTO countableProductRecipe VALUES(2, 1, 2);
+INSERT INTO countableProductRecipe VALUES(4, 1, 1);
+INSERT INTO countableProductRecipe VALUES(6, 1, 3);
+INSERT INTO countableProductRecipe VALUES(5, 1, 1);
+INSERT INTO uncountableProductRecipe VALUES(8, 1, 500);
+INSERT INTO uncountableProductRecipe VALUES(3, 1, 200);
+INSERT INTO liquidProductRecipe VALUES(2, 1, 125);
+INSERT INTO liquidProductRecipe VALUES(1, 1, 20);
+INSERT INTO binaryProductRecipe VALUES(9, 1);
+INSERT INTO binaryProductRecipe VALUES(2, 1);
+INSERT INTO binaryProductRecipe VALUES(3, 1);
+INSERT INTO binaryProductRecipe VALUES(5, 1);
+
+INSERT INTO countableProductRecipe VALUES(5, 2, 3);
+INSERT INTO uncountableProductRecipe VALUES(4, 2, 500);
+INSERT INTO uncountableProductRecipe VALUES(7, 2, 500);
+INSERT INTO uncountableProductRecipe VALUES(5, 2, 50);
+INSERT INTO uncountableProductRecipe VALUES(6, 2, 250);
+INSERT INTO liquidProductRecipe VALUES(3, 2, 250);
+INSERT INTO binaryProductRecipe VALUES(2, 2);
+INSERT INTO binaryProductRecipe VALUES(3, 2);
+
+INSERT INTO countableProductRecipe VALUES(5, 3, 2);
+INSERT INTO countableProductRecipe VALUES(7, 3, 1);
+INSERT INTO uncountableProductRecipe VALUES(8, 3, 800);
+INSERT INTO uncountableProductRecipe VALUES(4, 3, 100);
+INSERT INTO binaryProductRecipe VALUES(9, 3);
+INSERT INTO binaryProductRecipe VALUES(2, 3);
+INSERT INTO binaryProductRecipe VALUES(3, 3);
+
+INSERT INTO countableProductRecipe VALUES(1, 4, 1);
+INSERT INTO countableProductRecipe VALUES(5, 4, 1);
+INSERT INTO countableProductRecipe VALUES(9, 4, 6);
+INSERT INTO countableProductRecipe VALUES(10, 4, 1);
+INSERT INTO uncountableProductRecipe VALUES(2, 4, 500);
+INSERT INTO uncountableProductRecipe VALUES(9, 4, 200);
+INSERT INTO binaryProductRecipe VALUES(8, 4);
+INSERT INTO binaryProductRecipe VALUES(2, 4);
+INSERT INTO binaryProductRecipe VALUES(3, 4);
+
+INSERT INTO countableProductRecipe VALUES(12, 5, 3);
+INSERT INTO countableProductRecipe VALUES(8, 5, 3);
+INSERT INTO uncountableProductRecipe VALUES(10, 5, 240);
+INSERT INTO uncountableProductRecipe VALUES(11, 5, 80);
+INSERT INTO uncountableProductRecipe VALUES(5, 5, 280);
+INSERT INTO binaryProductRecipe VALUES(3, 5);
+
+INSERT INTO countableProductRecipe VALUES(8, 6, 2);
+INSERT INTO uncountableProductRecipe VALUES(10, 6, 150);
+INSERT INTO uncountableProductRecipe VALUES(11, 6, 200);
+INSERT INTO liquidProductRecipe VALUES(4, 6, 250);
+INSERT INTO binaryProductRecipe VALUES(3, 5);
+INSERT INTO binaryProductRecipe VALUES(3, 4);
+
+INSERT INTO countableProductHousehold VALUES(14, 7, 1);
+INSERT INTO countableProductHousehold VALUES(15, 7, 1);
+INSERT INTO countableProductHousehold VALUES(16, 7, 1);
+INSERT INTO countableProductHousehold VALUES(17, 7, 1);
+INSERT INTO countableProductHousehold VALUES(18, 7, 1);
+INSERT INTO countableProductHousehold VALUES(19, 7, 1);
+
+INSERT INTO uncountableProductHousehold VALUES(1, 1, 5);
+INSERT INTO uncountableProductHousehold VALUES(2, 1, 1);
+INSERT INTO uncountableProductHousehold VALUES(4, 1, 2);
+INSERT INTO uncountableProductHousehold VALUES(5, 1, 5);
+INSERT INTO uncountableProductHousehold VALUES(8, 1, 10);
+INSERT INTO uncountableProductHousehold VALUES(9, 1, 3);
+INSERT INTO uncountableProductHousehold VALUES(12, 1, 2);
+INSERT INTO uncountableProductHousehold VALUES(14, 1, 2);
+INSERT INTO uncountableProductHousehold VALUES(15, 1, 2);
+INSERT INTO uncountableProductHousehold VALUES(16, 1, 20);
+INSERT INTO uncountableProductHousehold VALUES(18, 1, 1);
+
+INSERT INTO liquidProductHousehold VALUES(1, 1, 1);
+INSERT INTO liquidProductHousehold VALUES(2, 1, 500);
+INSERT INTO liquidProductHousehold VALUES(3, 1, 150);
+INSERT INTO liquidProductHousehold VALUES(4, 1, 200);
+INSERT INTO liquidProductHousehold VALUES(5, 1, 250);
+INSERT INTO liquidProductHousehold VALUES(10, 1, 1000);
+INSERT INTO liquidProductHousehold VALUES(11, 1, 1000);
+
+INSERT INTO binaryProductHousehold VALUES(1, 1);
+INSERT INTO binaryProductHousehold VALUES(2, 1);
+INSERT INTO binaryProductHousehold VALUES(3, 1);
+INSERT INTO binaryProductHousehold VALUES(4, 1);
+INSERT INTO binaryProductHousehold VALUES(5, 1);
+INSERT INTO binaryProductHousehold VALUES(6, 1);
+INSERT INTO binaryProductHousehold VALUES(7, 1);
+INSERT INTO binaryProductHousehold VALUES(8, 1);
+INSERT INTO binaryProductHousehold VALUES(9, 1);
+
+
 
 
 
