c858fab0f232b47ee049716f91030e12a8fcf27a
klaudiaklis <30574178+klaudiaklis@users.noreply.github.com>
2017-08-03 23:06:57 +0200

    Getting user by login from DB implemented
    Connection with cookidea DB with some user dao implementation.

--- src/dao/DatabaseDao.java
+++ src/dao/DatabaseDao.java
@@ -1,11 +1,18 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import model.Household;
 
 public class DatabaseDao implements IDatabaseDao {
 
+	private static final String COOOKIDEA_CONNECTION_STRING = "jdbc:mysql://localhost/cookidea?user=root&password=root";
 	public DatabaseDao() {
-		
+		// TODO: implement me! ... or remove...
 	}
 	
 	@Override
@@ -15,7 +22,18 @@ public class DatabaseDao implements IDatabaseDao {
 
 	@Override
 	public Household getHouseholdByUser(String user) {
-		return null;
+		Household household = null;
+		try (Connection conn = DriverManager.getConnection(COOOKIDEA_CONNECTION_STRING)){
+			PreparedStatement prepareStatement = conn.prepareStatement("SELECT * FROM household WHERE login=?");
+			prepareStatement.setString(1, user);
+			ResultSet executeQuery = prepareStatement.executeQuery();
+			while (executeQuery.next()) {
+				household = new Household(executeQuery.getInt("id"), user, executeQuery.getString("password"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return household;
 	}
 
 }
